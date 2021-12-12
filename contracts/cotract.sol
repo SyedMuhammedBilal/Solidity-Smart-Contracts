cb43929fe93743d334aad110953071b9679b29df
"SyedMuhammedBilal" <"intergo878@gmail.com">
2021-12-12 12:48:17 +0500

    new files

---
+++ contracts/cotract.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MyFirstContract {
+    unit storageVariable;
+
+    function setValue(unit val) public {
+        storageVariable = val;
+    };
+
+    function getValue() public view return (unit) {
+        return storageVariable;
+    };
+};
