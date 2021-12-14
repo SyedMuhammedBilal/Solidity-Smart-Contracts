3cbd84f2d5cc4b7a79722a74003c0d52a3c3ef71
"SyedMuhammedBilal" <"intergo878@gmail.com">
2021-12-14 15:45:41 +0500

    setter function

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.8.0;
 
 // 0xd9145CCE52D386f254917e481eB44e9943F39138
 contract MyContract {
+    string public name = "Bilal";
 
+    function setName() public {
+        
+    }
 }
