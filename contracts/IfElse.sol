a1fd24104600035cf72cb1df74b9b1b6b39d1da1
"SyedMuhammedBilal" <"intergo878@gmail.com">
2021-12-19 18:25:00 +0500

    if else

---
+++ contracts/IfElse.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract IfElse {
+    uint32 public gasFee = 25;
+    uint256 public balance = 200;
+    bool public success = false;
+
+    function processTransaction() public {
+        balance = balance - 20;
+    }
+
+    function remainingBalance() public {
+        if (balance >= 180) {
+            success = true;
+        }
+    }
+}
---
+++ contracts/Units.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Units {
+    // 1 wei = 1
+    // 1gwei = 1e9
+    // 1 ether = 1e18 (1 ether = 1000000000000000000 wei)
+
+    // smart contracts works on wei units
+
+    uint256 costOfNFT = 0.05 ether;
+
+}
